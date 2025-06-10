a051b6169c60f731ea09dcfe5221a8cd143bb6f3
Nigel Atkinson <merlinblack@users.noreply.github.com>
2025-06-10 11:21:31 +1000

    Clarify which python hid lib

--- reset_mcp2221.py
+++ reset_mcp2221.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
+
+# hidapi (not hid)
 import hid
 
 mcp2221 = hid.device()
-mcp2221.open( 0x4d8, 0xdd )
+mcp2221.open(0x4D8, 0xDD)
 
-reset = b"\x70\xAB\xCD\xEF"
+reset = b"\x70\xab\xcd\xef"
 
-buff64 = b"\0" + reset + b"\0" * (64-len(reset))
+buff64 = b"\0" + reset + b"\0" * (64 - len(reset))
 
 mcp2221.write(buff64)
 
